8b477b081d55a7e4953201bbb1bc942411b62bc5
Ruoxin Jiang <rj2394@columbia.edu>
2017-12-01 00:55:10 -0500

    add vis

--- visualize.py
+++ visualize.py
@@ -16,12 +16,15 @@ def plot_component(results, results_train, component, ts_data, ts_idx, cs):
 def visualize_results(results, results_train, ts_data, ts_idx=0):
     ts = ts_data[ts_idx]
     cs = ['C1', 'C3', 'C5']
+    
+    # train and test split
+    plt.axvline(x=1., color='k',linestyle='--')
 
     # True data
     plt.plot(ts["history"]["t"], ts["history"]["y_scaled"].as_matrix(),
-             lw=1, color='C2')
+             lw=1, color='C2',label='y_true')
     plt.plot(ts["future"]["t"], ts["future"]["y_scaled"].as_matrix(),
-             lw=1, color='C2')
+             lw=1, color='C2',label='_nolegend_')
 
     for j, r in enumerate(results):
         df = r["predictions"][ts_idx]
@@ -29,8 +32,9 @@ def visualize_results(results, results_train, ts_data, ts_idx=0):
                  label="Model %d" % j, color=cs[j])
         if results_train is not None:
             df_train = results_train[j]["predictions"][ts_idx]
-            plt.plot(ts["history"]["t"], df_train["y"], color=cs[j])
+            plt.plot(ts["history"]["t"], df_train["y"], color=cs[j],label='_nolegend_')
     plt.legend(loc=2, prop={'size': 24})
+    #plt.legend(handles=[y_true_train,y_true_test])
     plt.show()
 
     plt.subplot(411)
